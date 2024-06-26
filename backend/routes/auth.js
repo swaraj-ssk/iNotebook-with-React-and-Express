e2637d4c84dc146d0632ab7b062bdb0cba0f4157
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-26 08:30:28 +0530

    Now Users data like name, email, password can be added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require("../models/Users.js");
 
-router.get('/', (req, res)=>{
-    obj={
-        a : 'thios',
-        num : 34
-    }
-    res.json(obj)
+// Create a user using POST : "/api/auth/". Do not require auth
+router.post('/', (req, res)=>{
+    console.log(req.body);
+    const user = User(req.body);
+    user.save();
+    res.send(req.body);
 })
 
 module.exports = router

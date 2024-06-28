7fe629ec4bd26784ef82eec3d3e9830402b5e82a
SwarajKalbande <swarajkalbande123@gmail.com>
2024-06-28 14:56:21 +0530

    Added Salt and Hashing for passwords and created Login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,7 +2,10 @@ const express = require('express');
 const router = express.Router();
 const User = require("../models/Users.js");
 const { check, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
+const JWT_SECRET = "MyName@isSwaraj";
 
 // Create a user using POST : "/api/auth/createuser". No login required
 router.post('/createuser',
@@ -24,21 +27,75 @@ router.post('/createuser',
             if (user) {
                 return res.status(400).json({ error: "Sorry a user with this email already exist." });
             }
+            const salt = await bcrypt.genSalt(10);
+            const secPass = await bcrypt.hash(req.body.password, salt);
             user = await User.create({
                 name: req.body.name,
                 email: req.body.email,
-                password: req.body.password
+                password: secPass
             })
-            // .then(user => res.json(user))
-            // .catch((err)=>{
-            //     console.log("Error has occured "+ err)
-            // });
-            res.json(user);
+            const data ={
+                user:{
+                    id : user.id
+                }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            console.log(authtoken);
+            res.json({
+                authtoken
+            })
 
-        } catch (error) {
+        } 
+        catch (error) {
             console.error(error.message);
             return res.status(500).send("Some error occured");
         }
-    })
+    }
+)
 
 module.exports = router
+
+//Authenticate User using POST "/api/auth/login/". No login required
+router.post('/login',
+    [
+        check('email', "Enter valid email").isEmail(),
+        check('password', "Password should be minimum 4 words").exists()
+    ],
+    async (req, res)=>{
+        const result = validationResult(req);
+        if (!result.isEmpty()) {
+            res.send({ errors: result.array() });
+        }
+
+        const{email, password} = req.body;
+
+        try{
+            let user = await User.findOne({email});
+            if(!user){
+                return res.status(400).json({error : "Please try to login with correct credentials"});
+            }
+
+            const passwordCompare = await bcrypt.compare(password, user.password);
+
+            if(!passwordCompare){
+                return res.status(400).json({error : "Please try to login with correct credentials"});
+            }
+            
+            const data ={
+                user:{
+                    id : user.id
+                }
+            }
+            const authtoken = jwt.sign(data, JWT_SECRET);
+            console.log(authtoken);
+            res.json({
+                authtoken
+            })
+
+        }
+        catch (error) {
+            console.error(error.message);
+            return res.status(500).send("Internal server error");
+        }
+    }
+)

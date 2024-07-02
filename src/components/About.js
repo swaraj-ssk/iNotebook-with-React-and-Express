38385925aff5935e3ac12f63194927648938810b
SwarajKalbande <swarajkalbande123@gmail.com>
2024-07-02 15:10:34 +0530

    Implmented Navbar active class using useLocation hook

--- src/components/About.js
+++ src/components/About.js
@@ -1,17 +1,11 @@
-import React, { useContext, useEffect } from 'react'
-import noteContext from '../context/notes/noteContext'
+import React from 'react'
+// import { useEffect, useContext } from 'react'
 
 const About = () => {
-  const a = useContext(noteContext)
-
-  useEffect(() => {
-    a.update();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [])
 
   return (
     <div>
-      This is About {a.state.name} and he is class {a.state.class}
+      This is about
     </div>
   )
 }
